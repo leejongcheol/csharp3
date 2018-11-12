067e19eee434bfdaf914aefb096f8423e4b4c67e
TopA-Teacher <TopA-Teacher@DESKTOP-HJNBQEA>
2018-11-12 14:20:11 +0900

    C# first App

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Emp {
+    int empno;
+    string name;
+    public void Work() { Console.WriteLine("Working~"); }
+}
+class EmpTest
+{  
+
+    public static void Main(string[] args)   //멤버, 메소드
+    {;
+        int i;   //System.Int32
+        i = 99;
+
+        int j = new int();
+        j = 99;
+
+        Console.WriteLine(i);
+    }
+}
