23f27c33d7858883bf8fda6a32e0b45077972fef
TopA-Teacher <TopA-Teacher@DESKTOP-HJNBQEA>
2018-11-12 15:16:41 +0900

    Add Decimal To Binary

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+class DecimalToBinary
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        if (0 < args.Length)
         {
+            foreach (string arg in args)
+            {
+                printBinary(int.Parse(arg));
+            }
         }
+        else
+        {
+            Console.Write("수를 넣으시오: ");
+            printBinary(int.Parse(Console.ReadLine()));
+        }
+    }
+
+    static void printBinary(int n)
+    {
+        Console.WriteLine("{0}\t--> {1}\n{2}\n", n, binary(n), bits(n));
+    }
+
+    static string binary(int n)
+    {
+        if (n == 0)
+        {
+            return "0";
+        }
+        string sign = "";
+        if (n < 0)
+        {
+            sign = "-";
+            n = -n;
+        }
+        string s = "";
+        for (; n != 0; n /= 2)
+        {
+            s = n % 2 + s;
+        }
+        return sign + s;
+    }
+
+    static string bits(int n)
+    {
+        string s = "";
+        for (uint bit = 0x80000000; bit != 0; bit >>= 1)
+        {
+            s += (n & bit) == 0 ? "0" : "1";
+        }
+        return s;
     }
 }
